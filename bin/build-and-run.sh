54df888753ad155a0eb69264f4014d46d1184f3f
Richard A. Lochner <lochner@clone1.com>
2025-05-17 14:51:10 -0500

    Made docker compose file distro and release generic. Made build-and-run.sh more generic, added build args and support for compose builds.

--- bin/build-and-run.sh
+++ bin/build-and-run.sh
@@ -2,12 +2,29 @@
 
 set -e
 
-BUILD="."
-[ -n "$1" ] && BUILD="-f Dockerfile-$1"
+if [ -z "$1" ]; then
+	cmd=$(basename "$0")
+	echo "Format: $cmd distro [release=latest] [build_with_compose=no]"
+	exit 1
+fi
+
+export DISTRO="${1:-fedora}"
+export RELEASE="${2:-latest}"
+export BUILD_DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+export VCS_REF="$(git rev-parse --short HEAD)"
+export BUILD_ARGS="--build-arg DISTRO=${DISTRO} --build-arg RELEASE=${RELEASE} --build-arg BUILD_DATE=${BUILD_DATE} --build-arg VCS_REF=${VCS_REF}"
 
 echo "Building the application."
-podman build -t lochnerr/systemd $BUILD
-if [ "$?" -ne 0 ]; then
+if [ "${3:-no}" = "no" ]; then
+	echo "Building with podman."
+	echo "podman build -t "lochnerr/$(basename "$PWD"):${DISTRO}-${RELEASE}" $BUILD_ARGS -f Dockerfile-"${DISTRO}""
+	podman build -t "lochnerr/$(basename "$PWD"):${DISTRO}-${RELEASE}" $BUILD_ARGS -f Dockerfile-"${DISTRO}" || err="yes"
+else
+	echo "Building with compose."
+	echo "podman-compose -f docker-compose.test.yml build $BUILD_ARGS"
+	podman-compose -f docker-compose.test.yml build $BUILD_ARGS || err="yes"
+fi
+if [ "$err" = "yes" ]; then
 	echo "ERROR: Build failed."
 	exit 1
 fi
