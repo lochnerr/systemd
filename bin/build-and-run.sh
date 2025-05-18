6ee6dd3dc69555b7be37534105af395bdab82df4
Richard A. Lochner <lochner@clone1.com>
2025-05-18 10:53:40 -0500

    Added a skip tests option. Added logic to find compose files.  Build with compose if the option is not set and there is a build section.

--- bin/build-and-run.sh
+++ bin/build-and-run.sh
@@ -4,9 +4,29 @@ set -e
 
 if [ -z "$1" ]; then
 	cmd=$(basename "$0")
-	echo "Format: $cmd distro [release=latest] [build_with_compose=no]"
+	echo "Format: $cmd distro [release=latest] [build_with_compose=no] [skip_tests=no]"
 	exit 1
 fi
+
+# Use a default compose if one exists.
+[ -e docker-compose.yml ]      && compose_file="docker-compose.yml"
+[ -e docker-compose.yaml ]     && compose_file="docker-compose.yaml"
+[ -e compose.yml ]             && compose_file="compose.yml"
+[ -e compose.yaml ]            && compose_file="compose.yaml"
+
+# From: https://docs.docker.com/docker-hub/repos/manage/builds/automated-testing/#set-up-automated-test-files
+# To set up your automated tests, create a docker-compose.test.yml file which defines a sut service that lists the tests to be run.
+# The docker-compose.test.yml file should be located in the same directory that contains the Dockerfile used to build the image.
+# So, prefer this file if it exists.
+[ -e docker-compose.test.yml ] && compose_file="docker-compose.test.yml"
+
+# Default to building with podman build, unless the build with compose option is yes.
+builder="podman"
+[ "$3" = "yes" ] && builder="compose"
+
+# If the build with compose option has not been set and the compose file has
+# a build section then default to building with compose.
+[ -z "$3" ] && grep -q "build:" "${compose_file}" 2>/dev/null && builder="compose"
 
 export DISTRO="${1:-fedora}"
 export RELEASE="${2:-latest}"
@@ -15,7 +35,7 @@ export VCS_REF="$(git rev-parse --short HEAD)"
 export BUILD_ARGS="--build-arg DISTRO=${DISTRO} --build-arg RELEASE=${RELEASE} --build-arg BUILD_DATE=${BUILD_DATE} --build-arg VCS_REF=${VCS_REF}"
 
 echo "Building the application."
-if [ "${3:-no}" = "no" ]; then
+if [ "${builder}" = "podman" ]; then
 	echo "Building with podman."
 	echo "podman build -t "lochnerr/$(basename "$PWD"):${DISTRO}-${RELEASE}" $BUILD_ARGS -f Dockerfile-"${DISTRO}""
 	podman build -t "lochnerr/$(basename "$PWD"):${DISTRO}-${RELEASE}" $BUILD_ARGS -f Dockerfile-"${DISTRO}" || err="yes"
@@ -27,6 +47,11 @@ fi
 if [ "$err" = "yes" ]; then
 	echo "ERROR: Build failed."
 	exit 1
+fi
+
+if [ "${4:-no}" = "yes" ]; then
+	echo "Skipping tests."
+	exit 0
 fi
 
 echo "Removing any unit test containers from a previous run."
